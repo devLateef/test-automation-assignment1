cd28baed183a851747d38227284fc3d3a48afb92
devLateef <lateefolatayo@gmail.com>
2024-01-19 11:25:55 +0100

    Added base classes

---
+++ src/test/java/Base/TestBase.java
@@ -0,0 +1,55 @@
+package Base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    public static WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+        String browser = "chrome";
+        if(browser.equals("chrome")){
+            driver = new ChromeDriver();
+            WebDriverManager.chromedriver().setup();
+        } else if(browser.equals("firefox")){
+            driver = new FirefoxDriver();
+            WebDriverManager.firefoxdriver().setup();
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+    public static void sleep(int seconds){
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    public static void getUrl(String url){
+        driver.get(url);
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("Inside before method annotation");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("Inside after method annotation");
+    }
+    @AfterClass
+    public void tearDown(){
+        System.out.println("Inside after class annotation");
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
